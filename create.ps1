aab9066cf243322b30ef77354f748342d335a45c
Jeroen Blaauw <72070984+JeroenBL@users.noreply.github.com>
2024-06-12 09:57:23 +0200

    Create create.ps1

---
+++ create.ps1
@@ -0,0 +1,32 @@
+############################################
+# HelloID-Conn-Prov-Target-GoodHabitz-Create
+# PowerShell V2
+############################################
+
+try {
+    if ($actionContext.DryRun -eq $true) {
+        Write-Information "[DryRun] correlate GoodHabitz account for: [$($personContext.Person.DisplayName)], will be executed during enforcement"
+        $outputContext.AccountReference = $actionContext.Data.EmailAddress
+        $outputContext.success = $true
+    }
+
+    if (-not($actionContext.DryRun -eq $true)) {
+        Write-Information 'Correlating GoodHabitz account'
+
+        $outputContext.AccountReference = $actionContext.Data.EmailAddress
+        $outputContext.AccountCorrelated = $true
+        $outputContext.success = $true
+        $outputContext.AuditLogs.Add([PSCustomObject]@{
+                Action  = 'CorrelateAccount'
+                Message = "Correlated account on field: [EmailAddress] with value: [$($actionContext.Data.EmailAddress)]"
+                IsError = $false
+            })
+    }
+} catch {
+    $outputContext.success = $false
+    Write-Warning "Error at Line '$($_.InvocationInfo.ScriptLineNumber)': $($_.InvocationInfo.Line). Error: $($_.Exception.Message)"
+    $outputContext.AuditLogs.Add([PSCustomObject]@{
+            Message = "Could not create or correlate GoodHabitz account. Error: $($_.Exception.Message)"
+            IsError = $true
+        })
+}
